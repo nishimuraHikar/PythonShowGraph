f93f58a963d633c8d8f3341b6a545eb56916deba
NishimuraHikar <h.nishimura@crl.epi.dendai.ac.jp>
2022-03-02 12:17:26 +0900

    multiplot

--- graph.py
+++ graph.py
@@ -51,3 +51,41 @@ def graph_2data(x_data, y1_data, y2_data, labelname1, labelname2):
     plt.legend()
     plt.savefig('NNresult.png') #PNGファイル生成
     plt.show()
+
+# マルチプロット2行1列
+def multiplot_2and1(x_data, y_data1a, y_data1b, y_data2a, y_data2b, labelname1, labelname2, labelname3, labelname4):
+    plt.rcParams['font.family'] = 'Times New Roman'
+    plt.rcParams["xtick.direction"] = "in"
+    plt.rcParams["ytick.direction"] = "in"
+    plt.rcParams["xtick.major.width"] = 1.5
+    plt.rcParams["ytick.major.width"] = 1.5
+    plt.rcParams["font.size"] = 16
+    plt.rcParams["axes.linewidth"] = 1.5
+
+    plt.figure(figsize=[9, 7])
+    # ax1 = fig.add_subplot(2, 1, 1)
+    # ax2 = fig.add_subplot(2, 1, 2)
+
+    plt.subplot(211)
+    plt.plot(x_data, y_data1a, c="darkorange", label=labelname1)
+    plt.plot(x_data, y_data1b, c="blue", label=labelname2)
+    plt.ylabel("Force[N]", fontname = 'Times New Roman')
+    plt.xlim(0, 60)
+    plt.ylim(-0.1, 0.2)
+    plt.axvspan(40, 60, color="gray", alpha=0.3)
+    plt.legend(loc = 'upper left')
+
+    plt.subplot(212)
+    plt.plot(x_data, y_data2a, c="darkorange", label=labelname3)
+    plt.plot(x_data, y_data2b, c="blue", label=labelname4)
+    plt.ylabel("Force[N]", fontname = 'Times New Roman')
+    plt.xlim(0, 60)
+    plt.ylim(-0.1, 0.2)
+
+    np.linspace(min(x_data), max(x_data), 100)
+    plt.xlabel("Time[sec]", fontname = 'Times New Roman')
+    plt.axvspan(40, 60, color="gray", alpha=0.3)
+    plt.legend(loc = 'upper left')
+
+    plt.tight_layout()
+    plt.show()
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import graph
 
-datafile = 'oct.csv'
+datafile = 'predict.csv'
 
 timedata = graph.ReadColumn(datafile,0)
-rawdata = graph.ReadColumn(datafile, 1)
-predictdata = graph.ReadColumn(datafile, 2)
+forcedatax = graph.ReadColumn(datafile, 1)
+predictdatax = graph.ReadColumn(datafile, 2)
+forcedatay = graph.ReadColumn(datafile, 3)
+predictdatay = graph.ReadColumn(datafile, 4)
 
 # graph.graph_1data(timedata, rawdata, "Raw Data")
-graph.graph_2data(timedata, rawdata, predictdata, "Raw Data", "Predict Data")
+# graph.graph_2data(timedata, rawdata, predictdata, "Raw Data", "Predict Data")
+graph.multiplot_2and1(timedata, forcedatax, predictdatax, forcedatay,predictdatay, "Actual X", "Predict X", "Actual Y", "Predict Y")
