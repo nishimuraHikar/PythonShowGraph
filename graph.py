0b484f7ebb0110062eea8654aa3aeb8873a7bb6c
NishimuraHikar <h.nishimura@crl.epi.dendai.ac.jp>
2022-03-03 04:18:01 +0900

    false

--- graph.py
+++ graph.py
@@ -49,7 +49,6 @@ def graph_2data(x_data, y1_data, y2_data, labelname1, labelname2):
     plt.xlabel("Time[sec]", fontname = 'Times New Roman')
     plt.ylabel("Force[N]", fontname = 'Times New Roman')
     plt.legend()
-# <<<<<<< HEAD
 #     plt.savefig('NNresult.png') #PNGファイル生成
 #     plt.show()
 
@@ -70,27 +69,24 @@ def multiplot_2and1(x_data, y_data1a, y_data1b, y_data2a, y_data2b, labelname1, 
     plt.subplot(211)
     plt.plot(x_data, y_data1a, c="darkorange", label=labelname1)
     plt.plot(x_data, y_data1b, c="blue", label=labelname2)
-    plt.ylabel("Force[N]", fontname = 'Times New Roman')
+    plt.ylabel("Force X[N]", fontname = 'Times New Roman')
     plt.xlim(0, 60)
-    plt.ylim(-0.1, 0.2)
-    plt.axvspan(40, 60, color="gray", alpha=0.3)
+    plt.ylim(-0.08, 0.1)
+    # plt.axvspan(40, 60, color="gray", alpha=0.3)
     plt.legend(loc = 'upper left')
 
     plt.subplot(212)
     plt.plot(x_data, y_data2a, c="darkorange", label=labelname3)
     plt.plot(x_data, y_data2b, c="blue", label=labelname4)
-    plt.ylabel("Force[N]", fontname = 'Times New Roman')
+    plt.ylabel("Force Y[N]", fontname = 'Times New Roman')
     plt.xlim(0, 60)
-    plt.ylim(-0.1, 0.2)
+    plt.ylim(-0.08, 0.1)
 
     np.linspace(min(x_data), max(x_data), 100)
     plt.xlabel("Time[sec]", fontname = 'Times New Roman')
-    plt.axvspan(40, 60, color="gray", alpha=0.3)
+    # plt.axvspan(40, 60, color="gray", alpha=0.3)
     plt.legend(loc = 'upper left')
 
     plt.tight_layout()
+    plt.savefig("false.pdf")
     plt.show()
-# =======
-#     # plt.savefig('NNresult.png') #PNGファイル生成
-# >>>>>>> b9eaac676acfc1c66dcc4038d5f1e117d2488184
-#     plt.show()
--- main.py
+++ main.py
@@ -1,36 +1,23 @@
 from time import time
 import graph
 
-# <<<<<<< HEAD
-datafile = 'predict.csv'
+# datafile1 = 'tozuka1.csv'
+datafile = 'false.csv'
 
-timedata = graph.ReadColumn(datafile,0)
-forcedatax = graph.ReadColumn(datafile, 1)
+# timedata = graph.ReadColumn(datafile1,0)
+# forcedatax = graph.ReadColumn(datafile1, 14)
+# forcedatax_ = graph.ReadColumn(datafile1, 31)
+# forcedatay = graph.ReadColumn(datafile1, 29)
+# forcedatay_ = graph.ReadColumn(datafile1, 32)
+
+timedata = graph.ReadColumn(datafile, 0)
+inputdatax = graph.ReadColumn(datafile, 1)
 predictdatax = graph.ReadColumn(datafile, 2)
-forcedatay = graph.ReadColumn(datafile, 3)
+inputdatay = graph.ReadColumn(datafile, 3)
 predictdatay = graph.ReadColumn(datafile, 4)
+
 
 # graph.graph_1data(timedata, rawdata, "Raw Data")
 # graph.graph_2data(timedata, rawdata, predictdata, "Raw Data", "Predict Data")
-graph.multiplot_2and1(timedata, forcedatax, predictdatax, forcedatay,predictdatay, "Actual X", "Predict X", "Actual Y", "Predict Y")
-# =======
-# datafile = 'predict_badassist.csv'
-
-# timedata = graph.ReadColumn(datafile,0)
-# teachdatax = graph.ReadColumn(datafile, 1)
-# predictdatax = graph.ReadColumn(datafile, 2)
-# seconddatax = graph.ReadColumn(datafile, 3)
-# Reliancex = graph.ReadColumn(datafile, 4)
-# teachdatay = graph.ReadColumn(datafile, 5)
-# predictdatay = graph.ReadColumn(datafile, 6)
-# seconddatay = graph.ReadColumn(datafile, 7)
-# Reliancey = graph.ReadColumn(datafile, 8)
-
-# # graph.graph_1data(timedata, rawdata, "Raw Data")
-# graph.graph_2data(timedata, teachdatax, predictdatax, "Teach_Forcex", "Predictionx")
-# graph.graph_2data(timedata, teachdatay, predictdatay, "Teach_Forcey", "Predictiony")
-# graph.graph_2data(timedata, seconddatax, predictdatax, "w/Assistx", "predictionx")
-# graph.graph_2data(timedata, seconddatay, predictdatay, "w/Assisty", "predictiony")
-# graph.graph_1data(timedata, Reliancex, "Reliancex")
-# graph.graph_1data(timedata, Reliancey, "Reliancey")
-# >>>>>>> b9eaac676acfc1c66dcc4038d5f1e117d2488184
+graph.multiplot_2and1(timedata, inputdatax, predictdatax, inputdatay, predictdatay, "Actual", "Predict", "Actual", "Predict")
+# graph.multiplot_2and1(timedata, forcedatax, forcedatax_, forcedatay, forcedatay_, "W/O Assist", "W/ Assist", "W/O Assist", "W/ Assist")
