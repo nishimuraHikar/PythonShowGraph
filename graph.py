a674904b077a558b4585d19c7b79cbddca7b675e
NisHikaru <56725059+nishimuraHikar@users.noreply.github.com>
2022-04-15 19:42:23 +0900

    long time no see! edited some design

--- graph.py
+++ graph.py
@@ -10,9 +10,8 @@ def ReadColumn(csv, line_num): #csvと列番号を指定
     array = csvdata.values
     return array
 
-# グラフ表示
-# 1つのグラフ
-def graph_1data(x_data, y_data, labelname):
+## 1つのグラフ
+def graph_1data(x_data, y_data, ylim, labelname):
     plt.rcParams['font.family'] = 'Times New Roman'
     plt.rcParams["xtick.direction"] = "in"
     plt.rcParams["ytick.direction"] = "in"
@@ -23,7 +22,7 @@ def graph_1data(x_data, y_data, labelname):
 
     plt.plot(x_data, y_data, c="darkorange", label=labelname)
     np.linspace(min(x_data), max(x_data), 100)
-    # plt.ylim(-1.55, 1.55)
+    plt.ylim(-ylim, ylim)
     # plt.ylim(min(y_data), max(y_data))
     plt.xlabel("Time[sec]", fontname = 'Times New Roman')
     plt.ylabel("Force[N]", fontname = 'Times New Roman')
@@ -54,7 +53,10 @@ def graph_2data(x_data, y1_data, y2_data, labelname1, labelname2):
 #     plt.show()
 
 # マルチプロット2行1列
-def multiplot_2and1(x_data, y_data1a, y_data1b, y_data2a, y_data2b, labelname1, labelname2, labelname3, labelname4):
+def multiplot_2and1(x_data,
+                    y_data1a, y_data1b, y_data2a, y_data2b,
+                    ylim,backdroung_start,background_end,
+                    labelname1, labelname2, labelname3, labelname4):
     plt.rcParams['font.family'] = 'Times New Roman'
     plt.rcParams["xtick.direction"] = "in"
     plt.rcParams["ytick.direction"] = "in"
@@ -62,35 +64,34 @@ def multiplot_2and1(x_data, y_data1a, y_data1b, y_data2a, y_data2b, labelname1, 
     plt.rcParams["ytick.major.width"] = 1.5
     plt.rcParams["font.size"] = 16
     plt.rcParams["axes.linewidth"] = 1.5
-
     plt.figure(figsize=[9, 7])
-    # ax1 = fig.add_subplot(2, 1, 1)
-    # ax2 = fig.add_subplot(2, 1, 2)
 
     plt.subplot(211)
     plt.plot(x_data, y_data1a, c="darkorange", label=labelname1)
     plt.plot(x_data, y_data1b, c="blue", label=labelname2)
-    plt.ylabel("Force X[N]", fontname = 'Times New Roman')
+    plt.ylabel("Force[N]", fontname = 'Times New Roman')
     plt.xlim(0, 60)
-    plt.ylim(-0.08, 0.1)
-    # plt.axvspan(40, 60, color="gray", alpha=0.3)
+    plt.ylim(-ylim, ylim)
+    plt.axvspan(backdroung_start, background_end, color="gray", alpha=0.3)
     plt.legend(loc = 'upper left')
 
     plt.subplot(212)
     plt.plot(x_data, y_data2a, c="darkorange", label=labelname3)
     plt.plot(x_data, y_data2b, c="blue", label=labelname4)
-    plt.ylabel("Force Y[N]", fontname = 'Times New Roman')
+    plt.ylabel("Force[N]", fontname = 'Times New Roman')
     plt.xlim(0, 60)
-    plt.ylim(-0.08, 0.1)
+    plt.ylim(-ylim, ylim)
+    plt.axvspan(backdroung_start, background_end, color="gray", alpha=0.3)
+    # plt.yticks(np.arange(-ylim, ylim, ystep))
+    plt.legend(loc = 'upper left')
 
     np.linspace(min(x_data), max(x_data), 100)
     plt.xlabel("Time[sec]", fontname = 'Times New Roman')
-    # plt.axvspan(40, 60, color="gray", alpha=0.3)
-    plt.legend(loc = 'upper left')
 
     plt.tight_layout()
-    plt.savefig("result.pdf")
+    plt.savefig("predict.pdf")
     plt.show()
+
 
 # ヒストグラム
 def histgram(data, binsnum, scale):
@@ -106,7 +107,34 @@ def histgram(data, binsnum, scale):
     ax.hist(data, bins = binsnum, density = True)
     X = np.linspace(-0.1, 0.1, 50)
     Y = norm.pdf(X, loc=0, scale=scale)
-    ax.plot(X, Y, c="red")
+    plt.ylabel("Frequency")
+    plt.show()
 
-    # plt.savefig("false.pdf")
-    # plt.show()
+
+def histgram2(data1, data2, binsnum, scale):
+    plt.rcParams['font.family'] = 'Times New Roman'
+    plt.rcParams["xtick.direction"] = "in"
+    plt.rcParams["ytick.direction"] = "in"
+    plt.rcParams["xtick.major.width"] = 1.5
+    plt.rcParams["ytick.major.width"] = 1.5
+    plt.rcParams["font.size"] = 16
+    plt.rcParams["axes.linewidth"] = 1.5
+    fig = plt.figure(figsize = [10,5])
+    ax = fig.add_subplot(121)
+    ax.hist(data1, bins = binsnum, density = True)
+    X = np.linspace(-0.1, 0.1, 50)
+    Y = norm.pdf(X, loc=0, scale=scale)
+    plt.xlim(-0.02, 0.02)
+    plt.ylim(0, 120)
+    plt.xlabel("Error X")
+    plt.ylabel("Frequency")
+
+    bx = fig.add_subplot(122)
+    bx.hist(data2, bins = binsnum, density = True)
+    plt.ylim(0, 120)
+    plt.xlim(-0.02, 0.02)
+    plt.xlabel("Error Y")
+    plt.ylabel("Frequency")
+    # ax.plot(X, Y, c="red")
+    plt.savefig("histgram.pdf")
+    plt.show()
--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-from time import time
 import graph
-import numpy as np
 
-# datafile1 = 'tozuka1.csv'
-# datafile = 'false.csv'
-datafile = 'predict.csv'
-
-# timedata = graph.ReadColumn(datafile1,0)
-# forcedatax = graph.ReadColumn(datafile1, 14)
-# forcedatax_ = graph.ReadColumn(datafile1, 31)
-# forcedatay = graph.ReadColumn(datafile1, 29)
-# forcedatay_ = graph.ReadColumn(datafile1, 32)
+datafile = 'csv/predictdata.csv'
 
 timedata = graph.ReadColumn(datafile, 0)
 inputdatax = graph.ReadColumn(datafile, 1)
@@ -19,10 +9,8 @@ inputdatay = graph.ReadColumn(datafile, 3)
 predictdatay = graph.ReadColumn(datafile, 4)
 
 
-# graph.graph_1data(timedata, rawdata, "Raw Data")
-# graph.graph_2data(timedata, rawdata, predictdata, "Raw Data", "Predict Data")
-# graph.multiplot_2and1(timedata, forcedatax, predictdatax, forcedatay,predictdatay, "Actual X", "Predict X", "Actual Y", "Predict Y")
-# graph.histgram(forcedatax-predictdatax, 1000, 100)
-graph.multiplot_2and1(timedata, inputdatax, predictdatax, inputdatay, predictdatay, "Actual", "Predict", "Actual", "Predict")
-# graph.multiplot_2and1(timedata, forcedatax, forcedatax_, forcedatay, forcedatay_, "W/O Assist", "W/ Assist", "W/O Assist", "W/ Assist")
-# graph.multiplot_2and1(timedata, forcedatax, predictdatax, forcedatay,predictdatay, "Actual X", "Predict X", "Actual Y", "Predict Y")
+graph.multiplot_2and1(timedata,
+                      inputdatax, predictdatax,
+                      inputdatay, predictdatay,
+                      1.5, 15.0, 60.0,
+                      "Actual X", "Predict X", "Actual Y", "Predict Y")
