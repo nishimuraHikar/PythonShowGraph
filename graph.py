ac62bb9d72cdf0251c623965a331a35c605c9302
NisHikaru <56725059+nishimuraHikar@users.noreply.github.com>
2022-04-18 09:05:06 +0900

    multiplot22 scatter

--- graph.py
+++ graph.py
@@ -17,6 +17,11 @@ def MeticulousReadColum(csv, line_num, StartLine, EndLine):
     array = SpecificLine.values
     return array
 
+def Edited(time, data, time_start):
+    for i in range(len(data)):
+        if(time[i] >= time_start):
+            return data[i]
+
 ## 1つのグラフ
 def graph_1data(x_data, y_data, ylim, labelname):
     plt.rcParams['font.family'] = 'Times New Roman'
@@ -36,7 +41,6 @@ def graph_1data(x_data, y_data, ylim, labelname):
     plt.legend()
     # plt.savefig('NNresult.png') #PNGファイル生成
     plt.show()
-
 
 # 2つのグラフ
 def graph_2data(x_data, y1_data, y2_data, labelname1, labelname2):
@@ -76,6 +80,7 @@ def multiplot_2and1(x_data,
     plt.subplot(211)
     plt.plot(x_data, y_data1a, c="darkorange", label=labelname1)
     plt.plot(x_data, y_data1b, c="blue", label=labelname2)
+    plt.fill_between(x_data, y_data1a, y_data1b, facecolor="lime", alpha=0.5)
     # plt.xlabel("subject1", fontname = 'Times New Roman')
     plt.ylabel("Force[N]", fontname = 'Times New Roman')
     plt.xlim(0, 60)
@@ -87,6 +92,7 @@ def multiplot_2and1(x_data,
     plt.subplot(212)
     plt.plot(x_data, y_data2a, c="darkorange", label=labelname3)
     plt.plot(x_data, y_data2b, c="blue", label=labelname4)
+    plt.fill_between(x_data, y_data2a, y_data2b, facecolor="lime", alpha=0.5)
     # plt.xlabel("subject2", fontname='Times New Roman')
     plt.ylabel("Force[N]", fontname = 'Times New Roman')
     plt.xlim(0, 60)
@@ -100,6 +106,122 @@ def multiplot_2and1(x_data,
 
     plt.tight_layout()
     plt.savefig("predict.pdf")
+    plt.show()
+
+# オブジェクト指向型
+def obj_multiplot_2and1(x_data,
+                    y_data1a, y_data1b, y_data2a, y_data2b,
+                    ylim,backdroung_start,background_end,
+                    labelname1, labelname2, labelname3, labelname4):
+    fig = plt.figure(figsize=[10, 8])
+    font = {'family' : 'Times New Roman', 'size' : 16}
+
+    # 2行1列のグラフ
+    ax1 = fig.add_subplot(211, xlim=(15, 60), ylim=(-ylim, ylim), ylabel="Force[N]")
+    ax2 = fig.add_subplot(212, xlim=(15, 60), ylim=(-ylim, ylim), ylabel="Force[N]")
+    fig.subplots_adjust(wspace=0.5, hspace= 0.5)
+
+    # １つ目のグラフ
+    ax1.plot(x_data, y_data1a, c="darkorange", label=labelname1)
+    ax1.plot(x_data, y_data1b, c="blue", label=labelname2)
+    ax1.set_ylabel("Force[N]", fontdict=font)
+    ax1.tick_params(direction = "in", width = "1.5")
+    ax1.fill_between(x_data.T[0], y_data1a.T[0], y_data1b.T[0], facecolor="lime", alpha=0.5)
+    # for i in range(len(y_data1a)):
+    #     if(y_data1a[i] - y_data1b[i] > 0):
+    #         ax1.fill_between(x_data.T[0], y_data1a[i].T[0], y_data1b[i].T[0], facecolor="lime", alpha=0.5)
+    #     elif(y_data1a[i] - y_data1b[i] <= 0):
+    #         ax1.fill_between(x_data.T[0], y_data1a[i].T[0], y_data1b[i].T[0], facecolor="deepskyblue", alpha=0.5)
+    # ax1.axvspan(backdroung_start, background_end, color="gray", alpha=0.3)
+    ax1.legend(fontsize=15, loc='upper center', bbox_to_anchor=(0.21, 1.0), borderaxespad=0, ncol=2)
+
+    # ２つ目のグラフ
+    ax2.plot(x_data, y_data2a, c="darkorange", label=labelname3)
+    ax2.plot(x_data, y_data2b, c="blue", label=labelname4)
+    ax2.set_ylabel("Force[N]", fontdict=font)
+    ax2.tick_params(direction = "in", width = "1.5")
+    ax2.fill_between(x_data.T[0], y_data2a.T[0], y_data2b.T[0], facecolor="lime", alpha=0.5)
+    # if(np.all(np.abs(y_data2a > y_data2b))):
+    #     ax2.fill_between(x_data.T[0], y_data2a.T[0], y_data2b.T[0], where=x_data.T[0] >= backdroung_start,
+    #                      facecolor="lime", alpha=0.5)
+    # elif(np.all(np.abs(y_data2a < y_data2b))):
+    #     ax2.fill_between(x_data.T[0], y_data2a.T[0], y_data2b.T[0], where=x_data.T[0] >= backdroung_start,
+    #                      facecolor="deepskyblue", alpha=0.5)
+    # ax2.axvspan(backdroung_start, background_end, color="gray", alpha=0.3)
+    ax2.legend(fontsize=15, loc='upper center', bbox_to_anchor=(0.21, 1.0), borderaxespad=0, ncol=2)
+
+    fig.tight_layout()
+    fig.savefig("predict.pdf")
+    plt.show()
+
+# マルチプロット2行2列
+def multiplot_2and2(x_data,
+                    y_data10, y_data11,
+                    y_data20,
+                    y_data30, y_data31,
+                    y_data40,
+                    x_start, x_end, ylim,
+                    labelname10, labelname11, labelname20, labelname21):
+    fig = plt.figure(figsize=[19, 10])
+    font = {'family' : 'Times New Roman', 'size' : 16}
+
+    # 2行2列のグラフ
+    ax1 = fig.add_subplot(221, xlim=(x_start, x_end), ylim=(-ylim, ylim))
+    ax2 = fig.add_subplot(223, xlim=(x_start, x_end), ylim=(-2.0, 2.0))
+    ax3 = fig.add_subplot(222, xlim=(x_start, x_end), ylim=(-ylim, ylim))
+    ax4 = fig.add_subplot(224, xlim=(x_start, x_end), ylim=(-2.0, 2.0))
+
+    # １つ目のグラフ
+    ax1.plot(x_data, y_data10, c="darkorange", label=labelname10)
+    ax1.plot(x_data, y_data11, c="blue", label=labelname11)
+    ax1.set_xlabel("Time[sec]", fontdict=font)
+    ax1.set_ylabel("Force[N]", fontdict=font)
+    ax1.hlines(y = 0, xmin = x_start, xmax = x_end, color = 'gray', linestyle = 'dashed', linewidth = 2)
+    ax1.tick_params(direction = "in", width = "1.5")
+    ax1.fill_between(x_data.T[0], y_data10.T[0], y_data11.T[0], facecolor="lime", alpha=0.5)
+    ax1.legend(fontsize=15, loc='upper center', bbox_to_anchor=(0.21, 1.0), borderaxespad=0, ncol=2)
+
+    # ２つ目のグラフ
+    ax2.plot(x_data, y_data20, c="deepskyblue")
+    ax2.set_xlabel("Time[sec]", fontdict=font)
+    ax2.set_ylabel("Reliance[N]", fontdict=font)
+    ax2.hlines(y=0, xmin=x_start, xmax=x_end, color='gray', linestyle='dashed', linewidth=2)
+    ax2.tick_params(direction = "in", width = "1.5")
+
+    # ３つ目のグラフ
+    ax3.plot(x_data, y_data30, c="darkorange", label=labelname20)
+    ax3.plot(x_data, y_data31, c="blue", label=labelname21)
+    ax3.set_xlabel("Time[sec]", fontdict=font)
+    ax3.set_ylabel("Force[N]", fontdict=font)
+    ax3.hlines(y=0, xmin=x_start, xmax=x_end, color='gray', linestyle='dashed', linewidth=2)
+    ax3.tick_params(direction = "in", width = "1.5")
+    ax3.fill_between(x_data.T[0], y_data30.T[0], y_data31.T[0], facecolor="lime", alpha=0.5)
+    ax3.legend(fontsize=15, loc='upper center', bbox_to_anchor=(0.21, 1.0), borderaxespad=0, ncol=2)
+
+    # ４つ目のグラフ
+    ax4.plot(x_data, y_data40, c="deepskyblue")
+    ax4.set_xlabel("Time[sec]", fontdict=font)
+    ax4.set_ylabel("Reliance[N]", fontdict=font)
+    ax4.hlines(y=0, xmin=x_start, xmax=x_end, color='gray', linestyle='dashed', linewidth=2)
+    ax4.tick_params(direction="in", width="1.5")
+
+    plt.tight_layout()
+    plt.savefig("Reliance.png")
+    plt.show()
+
+def Scatter(data1, data2):
+    plt.rcParams['font.family'] = 'Times New Roman'
+    plt.rcParams["xtick.direction"] = "in"
+    plt.rcParams["ytick.direction"] = "in"
+    plt.rcParams["xtick.major.width"] = 1.5
+    plt.rcParams["ytick.major.width"] = 1.5
+    plt.rcParams["font.size"] = 16
+    plt.rcParams["axes.linewidth"] = 1.5
+
+    np.linspace(min(data1), max(data1), 100)
+    plt.xlabel("Reliance[N]", fontname='Times New Roman')
+    plt.ylabel("Performance[m]", fontname= 'Times New Roman')
+    plt.scatter(data1, data2, s=10, c="blue", alpha=0.1, linewidths=2, edgecolors="blue")
     plt.show()
 
 
--- main.py
+++ main.py
@@ -1,18 +1,50 @@
 import graph
+import matplotlib.pyplot as plt
 
-datafile = 'csv/predict_kotaro.csv'
+datafile = 'csv/kotaro_predict.csv'
 
-timedata = graph.ReadColumn(datafile, 0)
-inputdatax = graph.ReadColumn(datafile, 1)
-# inputdatax2 = graph.MeticulousReadColum(datafile, 1, 100, 110)
-predictdatax = graph.ReadColumn(datafile, 2)
-inputdatay = graph.ReadColumn(datafile, 3)
-predictdatay = graph.ReadColumn(datafile, 4)
+# timedata = graph.ReadColumn(datafile, 0)
+# inputdatax = graph.ReadColumn(datafile, 1)
+# # inputdatax2 = graph.Edited(timedata, timedata, 15)
+# predictdatax = graph.ReadColumn(datafile, 2)
+# inputdatay = graph.ReadColumn(datafile, 3)
+# predictdatay = graph.ReadColumn(datafile, 4)
+# reliancex = inputdatax - predictdatax
+# reliancey = inputdatay - predictdatay
 
+timedata = graph.MeticulousReadColum(datafile, 0, 14500, 62527)
+inputdatax = graph.MeticulousReadColum(datafile, 1, 14500, 62527)
+predictdatax = graph.MeticulousReadColum(datafile, 2, 14500, 62527)
+inputdatay = graph.MeticulousReadColum(datafile, 3, 14500, 62527)
+predictdatay = graph.MeticulousReadColum(datafile, 4, 14500, 62527)
+reliancex = inputdatax - predictdatax
+reliancey = inputdatay - predictdatay
+rx = predictdatax / inputdatax
+ry = predictdatay / inputdatay
+perfomancex = graph.MeticulousReadColum(datafile, 7, 14500, 62527)
+perfomancey = graph.MeticulousReadColum(datafile, 8, 14500, 62527)
+
+# Editeddata = []
+# for i in range(len(timedata)):
+#     Editeddata[i] = graph.Edited(timedata[i], timedata[i], 15)
+#     print(Editeddata[i])
 # print(inputdatax2)
 
-graph.multiplot_2and1(timedata,
+# graph.obj_multiplot_2and1(timedata,
+#                       inputdatax, predictdatax,
+#                       inputdatay, predictdatay,
+#                       1.5, 15.0, 60.0,
+#                       "Actual X", "Predict X", "Actual Y", "Predict Y")
+
+graph.multiplot_2and2(timedata,
                       inputdatax, predictdatax,
+                      reliancex,
                       inputdatay, predictdatay,
-                      1.5, 15.0, 60.0,
-                      "Actual X", "Predict X", "Actual Y", "Predict Y")
+                      reliancey,
+                      15, 60, 1.5,
+                      "Actual x", "Predict x", "Actual y", "Predict y")
+
+# print(type(reliancex))
+# graph.Scatter(reliancex.T[0], perfomancex.T[0])
+# plt.scatter(reliancex.T[0], perfomancex.T[0])
+# plt.show()
